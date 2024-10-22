f88d5d788ab70d0a4ba7c12a0158ed57f620dbd5
João Pedro Miranda <jpedro10flp@gmail.com>
2024-10-22 18:57:53 -0300

    versao0.1

---
+++ MeuSiteEmMVC/Controllers/ContatoController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MeuSiteEmMVC.Controllers
+{
+    public class ContatoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
